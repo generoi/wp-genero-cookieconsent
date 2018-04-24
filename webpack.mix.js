5708dcafed0667adb2c5c5801aa90019d29d2862
Oskar Schöldström <m@oxy.fi>
2018-04-24 16:03:16 -0300

    allow jquery usage

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,11 @@ const mix = require('laravel-mix');
 
 mix.sass('assets/styles/main.scss', 'dist/')
   .js('assets/scripts/main.js', 'dist/')
+  .webpackConfig({
+    externals: {
+      'jquery': 'jQuery',
+    },
+  })
   .options({
     processCssUrls: false,
   });
