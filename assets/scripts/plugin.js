d42498233c23054ae2278e8c0574951ded0dd456
Oskar Schöldström <public@oxy.fi>
2020-10-21 10:29:07 -0300

    reload after revoking cookies with opt-in

--- assets/scripts/plugin.js
+++ assets/scripts/plugin.js
@@ -77,6 +77,7 @@ class GeneroCookieConsent {
 
     if (type === 'opt-in') {
       this.disableCookies();
+      location.reload();
     }
     if (type === 'opt-out') {
       this.enableCookies();
